6a6bae32557ec0e8b6ff9a08856a844fddbdae3b
Szymon Sulejczak <contact@szymonsulejczak.com>
2025-11-01 22:08:09 +0100

    add Course model with Tag relationship

--- main_app/models.py
+++ main_app/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Nazwa")
+    slug = models.SlugField(max_length=50, unique=True, verbose_name="Slug")
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tagi"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
